42f30203cf1c8fd591ac0127349e59ad17600060
furkan340 <71420790+furkan340@users.noreply.github.com>
2020-09-19 03:39:59 -0300

    Add files via upload

---
+++ Question7.java
@@ -0,0 +1,17 @@
+package day_2;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("  /\\  /\\  ");
+		System.out.println(" /__\\/__\\ ");
+		System.out.println("            ");
+		System.out.println("I am from \"Germany\"");
+		System.out.println("How is your day going on, \'good\'or \'bad\'");
+		
+		
+
+	}
+
+}
